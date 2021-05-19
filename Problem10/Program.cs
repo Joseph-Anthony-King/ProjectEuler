1a58eccb6c9337e646d1d2e53b641e6db9dceb59
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-05-19 08:22:26 -0700

    Rename problems

--- Problem10/Program.cs
+++ Problem10/Program.cs
@@ -3,63 +3,63 @@ using System.Collections.Generic;
 
 namespace Problem10
 {
-    class Program
+  class Program
+  {
+    // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+    // Find the sum of all the primes below two million.
+    static void Main(string[] args)
     {
-        // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+      var primes = new List<double>();
+      double result = 0;
 
-        // Find the sum of all the primes below two million.
-        static void Main(string[] args)
+      for (var i = 2; i < 2000000; i++)
+      {
+        if (IsPrime(i))
         {
-            var primes = new List<double>();
-            double result = 0;
-
-            for (var i = 2; i < 2000000; i++)
-            {
-                if (IsPrime(i))
-                {
-                    primes.Add(i);
-                }
-            }
-
-            foreach (var prime in primes)
-            {
-                result += prime;
-            }
-
-            Console.Write("Problem 10: ");
-            Console.WriteLine("The result is " + result);
-
-            Console.ReadLine();
+          primes.Add(i);
         }
+      }
 
-        public static bool IsPrime(double number)
-        {
-            if (number <= 1)
-            {
-                return false;
-            }
+      foreach (var prime in primes)
+      {
+        result += prime;
+      }
 
-            if (number == 2)
-            {
-                return true;
-            }
+      Console.Write("Problem 10: ");
+      Console.WriteLine("The result is " + result);
 
-            if (number % 2 == 0)
-            {
-                return false;
-            }
-
-            var boundary = (int)Math.Floor(Math.Sqrt(number));
-
-            for (int i = 3; i <= boundary; i += 2)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+      Console.ReadLine();
     }
+
+    public static bool IsPrime(double number)
+    {
+      if (number <= 1)
+      {
+        return false;
+      }
+
+      if (number == 2)
+      {
+        return true;
+      }
+
+      if (number % 2 == 0)
+      {
+        return false;
+      }
+
+      var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+      for (int i = 3; i <= boundary; i += 2)
+      {
+        if (number % i == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
 }
