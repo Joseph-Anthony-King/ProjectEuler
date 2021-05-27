453fa23d7bb924aa838ce532d14f3565131f1dd2
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-05-27 11:49:33 -0700

    add problem 20 answer

---
+++ Problem19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Problem19
+{
+    /* You are given the following information, but you may prefer to do some research for yourself.
+    
+       1 Jan 1900 was a Monday.
+       Thirty days has September,
+       April, June and November.
+       All the rest have thirty-one,
+       Saving February alone,
+       Which has twenty-eight, rain or shine.
+       And on leap years, twenty-nine.
+
+       A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+       How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)? */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDate = DateTime.Parse("01/01/1901");
+            var finalDate = DateTime.Parse("12/31/2000");
+            var result = 0;
+
+            do
+            {
+                if (currentDate.Day == 1 && currentDate.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    result++;
+                }
+
+                currentDate = currentDate.AddDays(1);
+
+            } while (currentDate <= finalDate);
+
+            Console.Write("Problem 19: ");
+            Console.WriteLine("The result is " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problem20/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem20
+{
+    /* n! means n × (n − 1) × ... × 3 × 2 × 1
+
+       For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+       and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+       
+       Find the sum of the digits in the number 100! */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Bang(new BigInteger(100));
+            result = SumOfDigits(result);
+
+            Console.Write("Problem 19: ");
+            Console.WriteLine("The result is " + result);
+            Console.ReadLine();
+        }
+
+        private static BigInteger Bang(BigInteger number)
+        {
+            BigInteger result = 1;
+
+            while (number > 0)
+            {
+                result *= number;
+                number -= 1;
+            }
+
+            return result;
+        }
+
+        private static BigInteger SumOfDigits(BigInteger number)
+        {
+            BigInteger result = 0;
+
+            var numberString = number.ToString();
+
+            var numberCharArray = numberString.ToCharArray();
+
+            foreach (var n in numberCharArray)
+            {
+                result += BigInteger.Parse(n.ToString());
+            }
+
+            return result;
+        }
+    }
+}
